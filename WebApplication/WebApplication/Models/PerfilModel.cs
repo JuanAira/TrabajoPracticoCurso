27cbc1243845d4a9e482ca8e95ebd4154d5e1ef2
Juan Aira <juanaira_01@hotmail.com>
2018-09-28 02:54:39 -0300

    Imagenes

--- WebApplication/WebApplication/Models/PerfilModel.cs
+++ WebApplication/WebApplication/Models/PerfilModel.cs
@@ -9,8 +9,17 @@ namespace WebApplication.Models
     public class PerfilModel
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [Required]
         public string Nombre { get; set; }
+
+        [Display(Name = "Apellido")]
+        [Required]
         public string Apellido { get; set; }
+
+        [Display(Name = "Nombre de usuario")]
+        [Required]
         public string NombreUsuario { get; set; }
 
      //Ver lo de la foto de perfil
